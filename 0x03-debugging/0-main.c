ad6f87192861a0d6fae6d977ed71def88981870e
Tebogo <tebogodipalett@gmail.com>
2022-10-20 14:47:23 -0700

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main -tests function that print if integer is positive or negative
+ * return: 0
+ */
+
+int main(void)
+{
+	int 1;
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
