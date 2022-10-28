bc75910670a18f3d012ea7fd63e6f2bc751d417e
Tebogo <tebogodipalett@gmail.com>
2022-10-27 23:58:09 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,21 +8,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int i;
-int k;
+	int i;
+	int k;
 
-i = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
-k = 0;
-while (src[k] != '\0')
-{
-dest[i] = src[k];
-i++;
-k++;
-}
-dest[i] = '\0';
-return (dest);
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	k = 0;
+	while (src[k] != '\0')
+	{
+		dest[i] = src[k];
+		i++;
+		k++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
